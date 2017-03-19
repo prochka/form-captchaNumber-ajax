883f71296ad9ca0ef787e37abbd0a44bfc680c80
Oleg <mr.dvorezky@gmail.com>
2017-03-19 15:07:07 +0300

    change captcha.js

--- js/captcha.js
+++ js/captcha.js
@@ -1,5 +1,6 @@
 (
- function($){
+  //алгоритм капчи
+ function($){ 
   $.fn.shuffle = function() {
    return this.each(function(){
      var items = $(this).children();
@@ -31,11 +32,49 @@
        else return arr;
    }
  })(jQuery);
+
+
  $(function() {
-   $("#sortable").sortable();
+   $("#sortable").sortable();//
    $("#sortable").disableSelection();
    $('ul').shuffle();
-   $("#button-blue").click(function(){
-     ($('ul').validate()) ? alert("Приветствую тебя, землянин!") : alert("Терминаторов на нашем сайте не обслуживают!");
-   });
+   $("#button-blue").click(function(){ //отправка формы
+
+    if($('ul').validate()){ //проверка капчи
+      $.ajax({
+        url: "js/form_processing.php",//url обработчика формы на сервере
+        type:     "POST", //Тип запроса
+        dataType: "html", //формат возвращаемых данных
+        data: $('#form1').serialize(), //приведение данных к виду a=1&b=2
+        success: function(data){  //Функция, которая будет вызвана в случае удачного завершения запроса к серверу. МОДАЛЬНОЕ ОКНО
+          var id = $('#dialog');  //Параметр - запрашиваемые данные 
+          var maskHeight = $(document).height();  //высота объекта document
+          var maskWidth = $(window).width();  
+          $('#mask').css({'width':maskWidth,'height':maskHeight}); //параметры затемняющего слоя
+          $('#mask').fadeIn(1000); 
+          $('#mask').fadeTo("slow",0.8); 
+          var winH = $(window).height();
+          var winW = $(window).width();
+          $(id).css('top',  winH/2-$(id).height()/2);//позиционирование модального окна с id = #dialog
+          $(id).css('left', winW/2-$(id).width()/2);// css-свойства заданы для #dialog в css-файле 
+          $(id).fadeIn(2000); 
+          $('.content').html(data);  //загружаем контент из файла-обработчика в модальное окно
+          $('.window .close').click(function (e) { 
+            e.preventDefault();
+            $('#mask, .window').hide();
+          }); 
+          $('#mask').click(function () {
+            $(this).hide();
+            $('.window').hide();
+          });
+        },
+        error: function(response) { //Если ошибка
+          document.getElementById('new2').innerHTML = "Ошибка при отправке формы";
+        }
+      });
+
+    } 
+    else
+      alert('Капчу заполни!');
+  })   
  });
